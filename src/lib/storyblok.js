d5db9a1bd9ad206d33e5f3e20eece50312608187
ManauTunjae <manau.tunjae@hotmail.com>
2026-09-01 21:07:22 +0200

    feat: ta bort Grid.js, Feature.js och Teaser.js fråm projektet pag. ej använda

--- src/lib/storyblok.js
+++ src/lib/storyblok.js
@@ -1,7 +1,4 @@
 import Page from '@/components/Page';
-import Feature from '@/components/Feature';
-import Grid from '@/components/Grid';
-import Teaser from '@/components/Teaser';
 import { apiPlugin, storyblokInit } from '@storyblok/react/rsc';
 
 export const getStoryblokApi = storyblokInit({
