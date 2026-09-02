db91f571747d00809f64aac81df2e375e6cdfa8a
ManauTunjae <manau.tunjae@hotmail.com>
2026-09-02 13:57:41 +0200

    feat: skapa About.jsx komponent

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import { storyblokEditable, renderRichText } from '@storyblok/react/rsc';
+
+export default function About({ blok }) {
+	return (
+		<section>
+			<h1>About page</h1>
+			<p>About is coming soon...</p>
+		</section>
+	);
+}
