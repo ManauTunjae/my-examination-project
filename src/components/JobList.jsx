1bf0dd32891f129a8f8b7de4a0c231ff549b1895
ManauTunjae <manau.tunjae@hotmail.com>
2026-09-02 15:07:33 +0200

    feat: hämta stories från Storyblok

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,7 +1,13 @@
 import { storyblokEditable, renderRichText } from "@storyblok/react/rsc";
 
 export default function JobList({blok}) {
-    
+    const storyblokApi = getStoryblokApi();
+    const { data } = await storyblokApi.get('cdn/stories', {
+        start_with: "jobs/",
+        content_type: "job-post",
+    })
+
+    const jobs = data.stories;
 
     return (
         <section>
