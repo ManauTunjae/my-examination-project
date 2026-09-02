f82264fe1001dd767d8a4f7fa6e1350175a2ffa6
ManauTunjae <manau.tunjae@hotmail.com>
2026-09-02 14:59:45 +0200

    feat: exportera JobList

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,1 +1,11 @@
 import { storyblokEditable, renderRichText } from "@storyblok/react/rsc";
+
+export default function JobList({blok}) {
+    
+
+    return (
+        <section>
+
+        </section>
+    )
+}
