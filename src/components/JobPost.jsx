e9189cc906631264b17991009bf3b0997e4b3c7c
ManauTunjae <manau.tunjae@hotmail.com>
2026-09-02 09:08:45 +0200

    feat: implementera JobPost-komponent med titel, metadata och rich text

--- src/components/JobPost.jsx
+++ src/components/JobPost.jsx
@@ -1,1 +1,16 @@
-import { storyblokEditable, renderRichText } from "@storyblok/react/rsc";
+import { storyblokEditable, renderRichText } from '@storyblok/react/rsc';
+
+export default function JobPost({ blok }) {
+	const richTextHtml = blok.content ? renderRichText(blok.content) : '';
+
+    return (
+        <sektion {...storyblokEditable(blok)}>
+            <h1>{blok.title}</h1>
+            <p>
+                <strong>{blok.department}</strong> - {blok.location}
+            </p>
+            <p>{blok.summary}</p>
+            <div dangerouslySetInnerHTML={{ __html: richTextHtml}}/>
+        </sektion>
+    )
+}
