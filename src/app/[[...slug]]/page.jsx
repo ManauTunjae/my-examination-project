95c241a058bb5be70dfbb990de885d047f9c7246
ManauTunjae <manau.tunjae@hotmail.com>
2026-09-04 16:21:00 +0200

    feat: uppdatera sidkomponenten för att inkludera sökparametrar och använda StoryblokServerComponent

--- src/app/[[...slug]]/page.jsx
+++ src/app/[[...slug]]/page.jsx
@@ -1,41 +1,46 @@
-import { StoryblokStory } from '@storyblok/react/rsc';
+import { StoryblokServerComponent } from '@storyblok/react/rsc';
 import { getStoryblokApi } from '@/lib/storyblok';
 import { notFound } from 'next/navigation';
 
-export default async function Page({ params }) {
+export default async function Page({ params, searchParams }) {
 	const { slug } = await params;
+	const resolvedSearchParams = await searchParams;
+	const department = resolvedSearchParams?.department ?? '';
+	const city = resolvedSearchParams?.city ?? '';
+	const q = resolvedSearchParams?.q ?? '';
 
-	let fullSlug = slug ? slug.join('/') : 'home';
-
-	let sbParams = {
-		version: 'draft',
-	};
-
+	let fullSlug = slug ? slug.join('/') : 'jobs';
 	const storyblokApi = getStoryblokApi();
 
 	let data;
 	try {
-		const response = await storyblokApi.get(
-			`cdn/stories/${fullSlug}`,
-			sbParams,
-		);
+		const response = await storyblokApi.get(`cdn/stories/${fullSlug}`, {
+			version: 'draft',
+		});
 		data = response.data;
 	} catch (error) {
 		notFound();
 	}
 
-	return <StoryblokStory story={data.story} />;
+	return (
+		<StoryblokServerComponent
+			blok={data.story.content}
+			department={department}
+			city={city}
+			q={q}
+		/>
+	);
 }
 
 export async function generateMetadata() {
 	const storyblokApi = getStoryblokApi();
 
-	const { data } = await storyblokApi.get('cdn/stories/home', {
+	const { data } = await storyblokApi.get('cdn/stories/jobs', {
 		version: 'draft',
 	});
 	const content = data.story.content;
 	return {
-		title: content.seo_title || data.story.name,
+		title: 'HEM-JOBPORTAL',
 		description: content.seo_description,
 	};
 }
