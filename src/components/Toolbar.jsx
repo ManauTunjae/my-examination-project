e67b61a24e617e658939874c579cf130a2636c15
ManauTunjae <manau.tunjae@hotmail.com>
2026-09-05 21:01:04 +0200

    feat: rendera nestedBlok Storyblok-blok dynamiskt

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,13 +1,26 @@
-import { storyblokEditable, StoryblokServerComponent } from '@storyblok/react/rsc';
+import {
+	storyblokEditable,
+	StoryblokServerComponent,
+} from '@storyblok/react/rsc';
 
 export default async function Toolbar({ blok, ...rest }) {
 	return (
-		<div {...storyblokEditable(blok)} className="w-full max-w-7xl mx-auto px-10 mb-8">
+		<div
+			{...storyblokEditable(blok)}
+			className="w-full max-w-7xl mx-auto px-10 mb-8"
+		>
 			<form
 				method="get"
 				action="/jobs"
 				className="flex flex-col md:flex-row items-stretch md:items-center gap-4"
 			>
+				{blok.blocks?.map((nestedBlok) => (
+					<StoryblokServerComponent
+						blok={nestedBlok}
+						key={nestedBlok._uid}
+						{...rest}
+					/>
+				))}
 			</form>
 		</div>
 	);
