297a59ea8ed0ed38602a28d937fc6ee1bd64d35e
ManauTunjae <manau.tunjae@hotmail.com>
2026-09-07 07:47:41 +0200

    featÖ uppdatera version från published till draft

--- src/app/[[...slug]]/page.jsx
+++ src/app/[[...slug]]/page.jsx
@@ -15,7 +15,7 @@ export default async function Page({ params, searchParams }) {
 	let data;
 	try {
 		const response = await storyblokApi.get(`cdn/stories/${fullSlug}`, {
-			version: 'published',
+			version: 'draft',
 		});
 		data = response.data;
 	} catch (error) {
@@ -36,7 +36,7 @@ export async function generateMetadata() {
 	const storyblokApi = getStoryblokApi();
 
 	const { data } = await storyblokApi.get('cdn/stories/jobs', {
-		version: 'published',
+		version: 'draft',
 	});
 	const content = data.story.content;
 	return {
